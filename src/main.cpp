f4ee86d205376dd148e75b5524fb1dede701bb16
neurott <neurott@protonmail.com>
2026-07-08 17:37:20 -0400

    Subiendo al repo

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <raylib.h>
+
+int main() 
+{
+
+    //game window
+    InitWindow(300,600,"Tetris");
+    SetTargetFPS(60);
+
+
+    Color darkBlue = {44,44,127,255};
+    
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing(); // begin de canvas drawiing
+        EndDrawing();
+    
+    }
+    
+
+
+
+
+    CloseWindow();
+
+
+    
+}
